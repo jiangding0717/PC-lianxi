02edc3e6a3b1ba4756e74bf72ad87756ff3b2988
jiangding0717 <jiangding0717@163.com>
2020-12-04 01:03:22 +0800

    feat:重写push与replace目的是为了让编程式导航点击时不报错

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,28 @@ import Login from '../views/Login'
 import Register from '../views/Register'
 import Search from '../views/Search'
 
+//重写push 和replace方法
+//目的是为了让编程式导航不报错
 Vue.use(VueRouter)
+const push = VueRouter.prototype.push
+const replace = VueRouter.prototype.replace
+VueRouter.prototype.push = function (location, onComplete, onAbort) {
+  // 如果用户想处理失败，就处理
+  if (onComplete && onAbort) {
+    return push.call(this, location, onComplete, onAbort);
+  }
+  // 如果用户不处理失败，给默认值：空函数
+  return push.call(this, location, onComplete, () => { });
+};
+
+VueRouter.prototype.replace = function (location, onComplete, onAbort) {
+  // 如果用户想处理失败，就处理
+  if (onComplete && onAbort) {
+    return replace.call(this, location, onComplete, onAbort);
+  }
+  // 如果用户不处理失败，给默认值：空函数
+  return replace.call(this, location, onComplete, () => { });
+};
 
 export default new VueRouter({
   routes: [
@@ -15,11 +36,17 @@ export default new VueRouter({
     },
     {
       path: '/login',
-      component: Login
+      component: Login,
+      meta: {
+        isFooterHide: true
+      }
     },
     {
       path: '/register',
-      component: Register
+      component: Register,
+      meta: {
+        isFooterHide: true
+      }
     },
     {
       name: 'search', //命名路由
